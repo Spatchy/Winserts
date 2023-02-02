b08adecf7a36356fb32bfc804400664b1159299c
Spatchy <spatchy@spatchy.net>
2023-02-02 20:41:26 +0100

    Initialise Winsert template

---
+++ your-winsert-name/payload.js
@@ -0,0 +1,9 @@
+const init = () => {
+  console.log(window.Winside.vars.winsertId)
+
+  // --- Your code here --- //
+  
+}
+
+if (document.readyState) init()
+else document.addEventListener("DOMContentLoaded", init)
