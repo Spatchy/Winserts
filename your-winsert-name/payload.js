db346882f3d11aa40337b71863c1ef5a01990f87
Spatchy <spatchy@spatchy.net>
2023-02-02 23:09:34 +0100

    New Winsert: Wikipedia

--- your-winsert-name/payload.js
+++
@@ -1,9 +0,0 @@
-const init = () => {
-  console.log(window.Winside.vars.winsertId)
-
-  // --- Your code here --- //
-  
-}
-
-if (document.readyState) init()
-else document.addEventListener("DOMContentLoaded", init)
