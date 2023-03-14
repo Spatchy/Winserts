da367afc0bb7afaaf3f1b00dabc32608872ecf8f
Spatchy <spatchy@spatchy.net>
2023-03-14 21:36:48 +0100

    Create new Winsert: ChatGPT

--- your-winsert-name/payload.js
+++
@@ -1,9 +0,0 @@
-const init = () => {
-  console.log(window.Winside.vars.winsertId)
-
-  // --- Your JS code here --- //
-  
-}
-
-if (document.readyState) init()
-else document.addEventListener("DOMContentLoaded", init)
